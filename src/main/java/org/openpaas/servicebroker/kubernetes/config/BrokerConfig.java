0aa5ae3949c099f056f881f34bb4024e388b552c
hyerin <hyerin@bluedigm.com>
2018-08-08 13:01:18 +0900

    필요 없는 것 삭제

--- src/main/java/org/openpaas/servicebroker/kubernetes/config/BrokerConfig.java
+++ src/main/java/org/openpaas/servicebroker/kubernetes/config/BrokerConfig.java
@@ -1,31 +1,13 @@
 package org.openpaas.servicebroker.kubernetes.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.autoconfigure.jdbc.EmbeddedDataSourceConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerConfig;
-
-import javax.sql.DataSource;
 
 
 /**
@@ -41,7 +23,6 @@ import javax.sql.DataSource;
 @EnableJpaRepositories("org.openpaas.servicebroker.kubernetes.repo")
 @EntityScan(value = "org.openpaas.servicebroker.kubernetes.model")
 public class BrokerConfig {
-    private static final Logger logger = LoggerFactory.getLogger( BrokerConfig.class );
 
     @Bean
     public PropertyPlaceholderConfigurer properties() {
