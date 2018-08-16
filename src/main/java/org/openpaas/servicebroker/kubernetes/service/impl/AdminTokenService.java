2fdbb7ccee1cf9f62c848675c7451d1d9baa5268
hyerin <hyerin@bluedigm.com>
2018-08-16 14:18:03 +0900

    로그 추가

--- src/main/java/org/openpaas/servicebroker/kubernetes/service/impl/AdminTokenService.java
+++ src/main/java/org/openpaas/servicebroker/kubernetes/service/impl/AdminTokenService.java
@@ -4,11 +4,15 @@ import org.openpaas.servicebroker.kubernetes.config.EnvConfig;
 import org.openpaas.servicebroker.kubernetes.repo.JpaAdminTokenRepository;
 import org.openpaas.servicebroker.kubernetes.service.RestTemplateService;
 import org.openpaas.servicebroker.kubernetes.service.SshService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminTokenService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AdminTokenService.class);
 
 	@Autowired
 	JpaAdminTokenRepository adminTokenRepository;
@@ -27,6 +31,7 @@ public class AdminTokenService {
 //	}
 	
 	public void setContext() {
+		logger.info("execute ssh command to caas master server to set admin token");
 		sshService.executeSsh("pwd");
 	}
 	
@@ -40,12 +45,14 @@ public class AdminTokenService {
 	}
 	
 	public void checkToken() {
+		logger.info("token check");
 		
 		// 토큰이 존재하지 않을 때
 		if(!tokenExist()) {
 			setContext();
 			// set Context를 한 뒤에 broker DB에 token 값을 저장해야 하는데 어떻게 함?
 			//adminTokenRepository.save(new JpaAdminToken(getAdminToken()));
+			logger.info("does not exist token");
 			return;
 		}
 		
