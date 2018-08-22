bb01458215e3cddf7e3b2d536a892915874f457b
hyerin <hyerin@bluedigm.com>
2018-08-22 13:17:38 +0900

    envConfig -> PropertyService로 교체

--- src/main/java/org/openpaas/servicebroker/kubernetes/service/PropertyService.java
+++ src/main/java/org/openpaas/servicebroker/kubernetes/service/PropertyService.java
@@ -23,6 +23,18 @@ public class PropertyService {
     
     @Value("${caas.cluster.user.password}")
     private String caasClusterUserPassword;
+	
+    @Value("${dashboard.url}")
+    private String dashboardUrl;
+    
+    @Value("${caas.url}")
+    private String caasUrl;
+    
+    @Value("${caas.admin-token}")
+    private String adminToken;
+    
+    @Value("${caas.common.url}")
+    private String commonUrl;
 
 	public String getCaasClusterUrl() {
 		return caasClusterUrl;
@@ -54,6 +66,38 @@ public class PropertyService {
 
 	public void setCaasClusterUserPassword(String caasClusterUserPassword) {
 		this.caasClusterUserPassword = caasClusterUserPassword;
+	}
+
+    public String getCaasUrl() {
+        return caasUrl;
+    }
+
+    public void setCaasUrl(String caasUrl) {
+        this.caasUrl = caasUrl;
+    }
+
+    public String getAdminToken() {
+        return adminToken;
+    }
+
+    public void setAdminToken(String adminToken) {
+        this.adminToken = adminToken;
+    }
+
+    public String getDashboardUrl(String serviceInstanceId) {
+        return dashboardUrl + serviceInstanceId;
+    }
+
+    public void setDashboardUrl(String dashboardUrl) {
+        this.dashboardUrl = dashboardUrl;
+    }
+
+	public String getCommonUrl() {
+		return commonUrl;
+	}
+
+	public void setCommonUrl(String commonUrl) {
+		this.commonUrl = commonUrl;
 	}
 
 }
