7c04796b06b0314abec45b0797eb249b71ddcf24
hrjin <hyunlee@bluedigm.com>
2019-11-15 18:39:52 +0900

    CaaS Master 어드민 토큰 조회(Bean 추가)

---
+++ src/main/java/org/openpaas/servicebroker/caas/config/InitConfig.java
@@ -0,0 +1,41 @@
+package org.openpaas.servicebroker.caas.config;
+
+import org.openpaas.servicebroker.caas.service.impl.AdminTokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * 기본 Spring boot packages의 component 검색하고,
+ * jpa사용을 위해서 @EntityScan, @EnableJpaRepositories를 추가
+ *
+ * @author 박혜린
+ * @version 20180724
+ * @since 2018.07.24
+ */
+@Component
+public class InitConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(InitConfig.class);
+
+
+    @Autowired
+    AdminTokenService adminTokenService;
+
+    @Bean
+    public List<String> createAdminToken() {
+        List<String> empty = new ArrayList<>();
+
+        logger.info("createAdminToken======================================================Start");
+        adminTokenService.setContext();
+        logger.info("createAdminToken======================================================End");
+
+        return empty;
+    }
+}
